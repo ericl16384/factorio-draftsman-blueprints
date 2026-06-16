1aa6fc883330c7ac3c7fb4e160272b3a6504b006
Eric <ericl16384@gmail.com>
2026-06-16 09:44:34 -0700

    make prototype/entity format

--- distribute_rectangles/main.py
+++ distribute_rectangles/main.py
@@ -6,73 +6,68 @@ import numpy as np
 
 
 
-class FactorioSubassembly:
+class FactorioSubassemblyPrototype:
     def __init__(self, width, height, inputs, outputs) -> None:
         self.width = width
         self.height = height
         self.inputs = inputs
         self.outputs = outputs
+
+class FactorioSubassemblyEntity:
+    def __init__(self, prototype, x, y) -> None:
+        self.prototype = prototype
+        self.x = x
+        self.y = y
     
-    def apply_to_canvas(self, canvas, x, y):
+    def apply_to_canvas(self, canvas):
         # Map x to columns and y to rows
-        canvas[y : y + self.height, x : x + self.width] = 0
-        for x, y in self.outputs:
-            canvas[y, x] = 0
-        for x, y in self.inputs:
-            canvas[y, x] = 0
+        canvas[self.y : self.y + self.prototype.height, self.x : self.x + self.prototype.width] = 0
+        for proto_x, proto_y in self.prototype.outputs:
+            canvas[self.y + proto_y, self.x + proto_x] = 0
+        for proto_x, proto_y in self.prototype.inputs:
+            canvas[self.y + proto_y, self.x + proto_x] = 0
 
 
-subassemblies = {
-    "foo": FactorioSubassembly(6, 7, [[5, 6]], [[6, 0]])
+
+
+
+
+subassembly_prototypes = {
+    "foo": FactorioSubassemblyPrototype(6, 7, [[5, 6]], [[6, 0]])
 }
 
-
-
-# Format: [x, y, width, height]
-rectangles = [
-    [1, 1, 3, 2],  # Rect 1: Starts at (1,1), width 3, height 2
-    [5, 4, 4, 5],  # Rect 2: Starts at (5,4), width 4, height 5
-]
+subassembly_entities = []
+subassembly_entities.append(FactorioSubassemblyEntity(subassembly_prototypes["foo"], x=10, y=10))
+subassembly_entities.append(FactorioSubassemblyEntity(subassembly_prototypes["foo"], x=30, y=15))
 
 
 
 
 canvas = np.ones((40, 100), dtype=bool)
 
-
-for x, y, width, height in rectangles:
-    # Map x to columns and y to rows
-    canvas[y : y + height, x : x + width] = 0
+for subassembly_entity in subassembly_entities:
+    subassembly_entity.apply_to_canvas(canvas)
 
 
 
+for i in range(1):
+    # print(canvas)
 
+    grid = Grid(matrix=canvas)
 
+    # print(grid.grid_str())
 
+    start = grid.node(0, 0)
+    end = grid.node(8, 8)
 
-canvas[0, 9] = 0
-canvas[1, 8] = 0
+    # print(start, end)
 
-canvas[0, 0] = 0
-canvas[9, 9] = 0
+    finder = BiAStarFinder()
 
-# print(canvas)
+    # Calculate the path and number of runs
+    path, runs = finder.find_path(start, end, grid)
 
-grid = Grid(matrix=canvas)
-
-# print(grid.grid_str())
-
-start = grid.node(0, 0)
-end = grid.node(9, 9)
-
-# print(start, end)
-
-finder = BiAStarFinder()
-
-# Calculate the path and number of runs
-path, runs = finder.find_path(start, end, grid)
-
-# Print the result
-# print("Path found:", path)
-print(grid.grid_str(path=path, start=start, end=end))
+    # Print the result
+    # print("Path found:", path)
+    print(grid.grid_str(path=path, start=start, end=end))
 
